f0e30ad893d4c6c1a8c8dce7bb176992fc90a7d2
Sujonmia5 <mdsujon673738@gmail.com>
2022-11-24 23:17:10 +0600

    oders collection api create

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors')
@@ -20,19 +20,52 @@ async function run() {
     try {
         const Users = client.db('MyCarDatabase').collection('users')
         const CarsData = client.db('MyCarDatabase').collection('carsData')
+        const Category = client.db('MyCarDatabase').collection('CategoryData')
+        const Orders = client.db('MyCarDatabase').collection('Orders')
+
+        app.post('/orders', async (req, res) => {
+            const order = req.body
+            const result = await Orders.insertOne(order)
+            res.send(result)
+        })
+
+        app.put('/bookings', async (req, res) => {
+            const id = req.query.id;
+            const filter = { _id: ObjectId(id) }
+            const option = { upsert: true }
+            const docs = {
+                $set: { booking: true }
+            }
+            const result = await CarsData.updateOne(filter, docs, option)
+            res.send(result)
+        })
+
+        app.get('/category', async (req, res) => {
+            const query = {}
+            const result = await Category.find(query).toArray()
+            res.send(result)
+        })
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await Users.insertOne(user)
             res.send(result)
         })
+        app.post('/cars', async (req, res) => {
+            const data = req.body;
+            const date = new Date()
+            const query = {
+                ...data,
+                date,
+            }
+            const result = await CarsData.insertOne(query)
+            res.send(result)
+        })
 
         app.get('/cars/data/:id', async (req, res) => {
-            const categoryId = parseInt(req.params.id)
+            const brand = req.params.id
             const query = {
-                Category_id: categoryId
+                brand: brand
             }
-            console.log(query);
-
             const result = await CarsData.find(query).toArray()
             res.send(result)
         })
